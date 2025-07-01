e1d92de3048f5f0b1160462c02dab3374ba57284
Benjamin <bybenh@gmail.com>
2025-07-02 01:25:25 +0300

    3.1-3.6

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -8,8 +8,50 @@ let persons = [
   { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
 ]
 
+app.use(express.json())
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' })
+  }
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+  const info = `Phonebook has info for ${persons.length} people<br>${new Date()}`
+  res.send(info)
 })
 
 const PORT = 3001
