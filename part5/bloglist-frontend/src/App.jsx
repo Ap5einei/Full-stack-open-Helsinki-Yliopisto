75aed6f35fb8ac8779feefe51a9daab4cb0c9e16
Benjamin <bybenh@gmail.com>
2025-07-07 01:22:02 +0300

    5.3 allow logged in users to add new blogs

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,62 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+
+  const blogForm = () => (
+  <form onSubmit={addBlog}>
+    <div>
+      title
+      <input
+        type="text"
+        value={title}
+        name="Title"
+        onChange={({ target }) => setTitle(target.value)}
+      />
+    </div>
+    <div>
+      author
+      <input
+        type="text"
+        value={author}
+        name="Author"
+        onChange={({ target }) => setAuthor(target.value)}
+      />
+    </div>
+    <div>
+      url
+      <input
+        type="text"
+        value={url}
+        name="Url"
+        onChange={({ target }) => setUrl(target.value)}
+      />
+    </div>
+    <button type="submit">create</button>
+  </form>
+)
+
+const addBlog = async (event) => {
+  event.preventDefault()
+  try {
+    const newBlog = {
+      title,
+      author,
+      url,
+    }
+    const returnedBlog = await blogService.create(newBlog)
+    setBlogs(blogs.concat(returnedBlog))
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  } catch (error) {
+    alert('Error adding blog')
+  }
+}
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -20,6 +76,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -33,6 +90,7 @@ const App = () => {
         'loggedBlogappUser', JSON.stringify(user)
       )
       setUser(user)
+      blogService.setToken(user.token)
       setUsername('')
       setPassword('')
     } catch (error) {
@@ -70,22 +128,24 @@ const App = () => {
   )
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {user === null
-        ? loginForm()
-        : <div>
-            <p>
-              {user.name} logged in
-              <button onClick={handleLogout}>logout</button>
-            </p>
-            {blogs.map(blog =>
-              <Blog key={blog.id} blog={blog} />
-            )}
-          </div>
-      }
-    </div>
-  )
+  <div>
+    <h2>blogs</h2>
+    {user === null
+      ? loginForm()
+      : <div>
+          <p>
+            {user.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </p>
+          {blogForm()}
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+    }
+  </div>
+)
+
 }
 
 export default App
