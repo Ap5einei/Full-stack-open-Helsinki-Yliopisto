cfe957aa0fb0a18b2ddc885d01c7189d3af3b769
Benjamin <bybenh@gmail.com>
2025-07-07 02:53:27 +0300

    5.6-5.7 added view/hide button and detailed view to Blog component

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const Blog = ({ blog, handleLike, handleRemove, user }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible &&
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button onClick={() => handleLike(blog)}>like</button>
+          </div>
+          <div>{blog.user && blog.user.name}</div>
+          {user && blog.user && user.username === blog.user.username &&
+            <button onClick={() => handleRemove(blog)}>delete</button>
+          }
+        </div>
+      }
+    </div>
+  )
+}
+
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  handleLike: PropTypes.func,
+  handleRemove: PropTypes.func,
+  user: PropTypes.object
+}
 
 export default Blog
