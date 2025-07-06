c0ca4678a776532a48812085cd10068ed17cc60e
Benjamin <bybenh@gmail.com>
2025-07-07 01:08:57 +0300

    5.1Add login form and authentication logic

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +17,60 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+  event.preventDefault()
+  try {
+    const user = await loginService.login({
+      username, password,
+    })
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  } catch (error) {
+    alert('Wrong credentials') // voit korvata tämän paremmalla notifikaatiolla myöhemmin
+  }
+}
+
+const loginForm = () => (
+  <form onSubmit={handleLogin}>
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      username
+      <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
     </div>
-  )
+    <div>
+      password
+      <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>
+)
+
+
+  return (
+  <div>
+    <h2>blogs</h2>
+    {user === null
+      ? loginForm()
+      : <div>
+          <p>{user.name} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+    }
+  </div>
+)
+
 }
 
 export default App
