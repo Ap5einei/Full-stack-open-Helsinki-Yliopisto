eb8b866fce165382961a0f83d03baca89de8e622
Benjamin <bybenh@gmail.com>
2025-07-07 22:04:09 +0300

    6.4

--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,29 @@
+// Alkuperäiset anekdootit
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+// Id:n generointi
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+// Muuttaa tekstin anekdootti-olioksi
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+// Alkutila taulukkona olioita
+const initialState = anecdotesAtStart.map(asObject)
+
+// Reducer-funktio
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
@@ -16,6 +42,8 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+
+// Action creator: äänestys
 export const voteAnecdote = (id) => {
   return {
     type: 'VOTE',
@@ -23,6 +51,7 @@ export const voteAnecdote = (id) => {
   }
 }
 
+// Action creator: uuden anekdootin luonti
 export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
@@ -33,3 +62,5 @@ export const createAnecdote = (content) => {
     }
   }
 }
+
+export default reducer
