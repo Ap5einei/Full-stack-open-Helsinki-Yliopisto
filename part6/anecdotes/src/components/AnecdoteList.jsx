b7b357ed9bcdfaab9bafee2aca23dfba97d8b9bf
Benjamin <bybenh@gmail.com>
2025-07-07 22:36:30 +0300

    6.3-6.8 Redux

---
+++ part6/anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state =>
+    [...state.anecdotes].sort((a, b) => b.votes - a.votes)
+  )
+
+  const vote = (id, content) => {
+    dispatch(voteAnecdote(id))
+    dispatch(setNotification(`you voted '${content}'`))
+    setTimeout(() => {
+      dispatch(setNotification(''))
+    }, 5000)
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
---
+++ part6/anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,25 @@
+const notificationReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.payload
+    case 'CLEAR_NOTIFICATION':
+      return ''
+    default:
+      return state
+  }
+}
+
+export const setNotification = (message) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    payload: message
+  }
+}
+
+export const clearNotification = () => {
+  return {
+    type: 'CLEAR_NOTIFICATION'
+  }
+}
+
+export default notificationReducer
