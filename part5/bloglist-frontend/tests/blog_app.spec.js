5c475cc0a4f54078384c5e7ab6175558b745e957
Benjamin <bybenh@gmail.com>
2025-07-07 16:04:49 +0300

    refactoring code

--- part5/bloglist-frontend/tests/blog_app.spec.js
+++ part5/bloglist-frontend/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
 
 test('user can log in', async ({ page }) => {
   await page.goto('http://localhost:5173');
