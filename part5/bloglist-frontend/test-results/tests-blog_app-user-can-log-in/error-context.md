cb24f57a781811c7f9656145c40c8a60e8f459ef
Benjamin <bybenh@gmail.com>
2025-07-07 16:24:37 +0300

    testing

---
+++ part5/bloglist-frontend/test-results/tests-blog_app-user-can-log-in/error-context.md
@@ -0,0 +1,10 @@
+# Page snapshot
+
+```yaml
+- heading "blogs" [level=2]
+- text: username
+- textbox
+- text: password
+- textbox
+- button "login"
+```
--- part5/bloglist-frontend/tests/blog_app.test.js
+++ part5/bloglist-frontend/tests/blog_app.test.js
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-
 test('user can log in', async ({ page }) => {
   await page.goto('http://localhost:5173');
+  await expect(page.getByPlaceholder('Username')).toBeVisible();
   await page.getByPlaceholder('Username').fill('mluukkai');
   await page.getByPlaceholder('Password').fill('salainen');
   await page.getByRole('button', { name: 'login' }).click();
   await expect(page.getByText('mluukkai logged in')).toBeVisible();
 });
-
