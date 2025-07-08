c2e2e29e18d5ace7d3a98eb8f4945382f9b1f16f
Benjamin <bybenh@gmail.com>
2025-07-08 16:32:22 +0300

    6.1-6.9 debugging

--- part7/bloglist/backend/controllers/blogs.js
+++ part7/bloglist/backend/controllers/blogs.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const userExtractor = require('../utils/userExtractor')
 
+// Hae kaikki blogit, populate käyttäjän tiedot
 blogsRouter.get('/', async (request, response, next) => {
   try {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -12,6 +13,7 @@ blogsRouter.get('/', async (request, response, next) => {
   }
 })
 
+// Luo uusi blogi, vaatii tunnistautumisen
 blogsRouter.post('/', userExtractor, async (request, response, next) => {
   try {
     const user = request.user
@@ -28,6 +30,46 @@ blogsRouter.post('/', userExtractor, async (request, response, next) => {
     const populatedBlog = await Blog.findById(savedBlog._id).populate('user', { username: 1, name: 1 })
 
     response.status(201).json(populatedBlog)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Päivitä blogi (esim. tykkäysten päivitys)
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const { title, author, url, likes, user } = request.body
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      { title, author, url, likes, user },
+      { new: true, runValidators: true, context: 'query' }
+    ).populate('user', { username: 1, name: 1 })
+
+    if (updatedBlog) {
+      response.json(updatedBlog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Poista blogi, vaatii tunnistautumisen ja omistajuuden tarkistuksen
+blogsRouter.delete('/:id', userExtractor, async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+      return response.status(404).json({ error: 'Blog not found' })
+    }
+
+    // Tarkistetaan, että poistaja on blogin omistaja
+    if (blog.user.toString() !== request.user._id.toString()) {
+      return response.status(401).json({ error: 'Unauthorized: only the creator can delete the blog' })
+    }
+
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   } catch (error) {
     next(error)
   }
