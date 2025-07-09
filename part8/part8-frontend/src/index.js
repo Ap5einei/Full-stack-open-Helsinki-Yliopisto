32e42d8c1899b7be8473a4256de5b8feee9154e5
Benjamin <bybenh@gmail.com>
2025-07-09 07:37:06 +0300

    Add book creation and login functionality
    Fix "not authenticated" error
    
    Move queries.js inside src directory
    
    Update UI to Houston Inc. inspired style
    
    Add ApolloProvider to index.js
    
    Store token in localStorage after login
    
    Fix import paths to use ./queries
    
    Add pink-purple gradient background
    
    Refactor App.js, remove unused client variable
    
    Improve login error handling

--- part8/part8-frontend/src/index.js
+++ part8/part8-frontend/src/index.js
@@ -1,10 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import App from './App'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
